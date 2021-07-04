14a13c78ecc2fa725dc8f78d0acdab29bfd5a84e
Bob <bob@bitoex.com>
2021-07-04 17:49:14 +0800

    add fetch ethereum gas tracker

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,17 +1,34 @@
+import { useState, useEffect } from 'react'
 import { Helmet } from 'react-helmet'
 import { Box, Container, Grid } from '@material-ui/core'
 import Budget from 'src/components/dashboard//Budget'
 import EthereumGas from 'src/components/dashboard//EthereumGas'
 import TasksProgress from 'src/components/dashboard//TasksProgress'
 import TotalCustomers from 'src/components/dashboard//TotalCustomers'
-import { red, blue, green } from '@material-ui/core/colors'
+import { red, blue, green, orange } from '@material-ui/core/colors'
 
 const Dashboard = () => {
-  const gweiList = [
-    { gwei: 35, level: 'Low', bg: green[600] },
-    { gwei: 28, level: 'Average', bg: blue[600] },
-    { gwei: 52, level: 'High', bg: red[600] }
-  ]
+  const [gasTracker, setGasTracker] = useState({
+    LastBlock: null,
+    SafeGasPrice: null,
+    ProposeGasPrice: null,
+    FastGasPrice: null
+  })
+
+  useEffect(() => {
+    fetch(
+      'https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=DGIQU4UGTA2UW72KABXMZ6A1BG5EAV1XW1'
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          if (result?.message === 'OK') setGasTracker(result?.result)
+        },
+        (error) => {
+          console.log('fetch api error', error)
+        }
+      )
+  }, [])
 
   return (
     <>
@@ -26,11 +43,18 @@ const Dashboard = () => {
         }}>
         <Container maxWidth={false}>
           <Grid container spacing={3}>
-            {gweiList.map(({ gwei, level, bg }) => (
-              <Grid key={level} item lg={3} sm={6} xl={3} xs={12}>
-                <EthereumGas gwei={gwei} level={level} bg={bg} />
-              </Grid>
-            ))}
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <EthereumGas gwei={gasTracker?.SafeGasPrice} level={'Low'} bg={green[600]} />
+            </Grid>
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <EthereumGas gwei={gasTracker?.ProposeGasPrice} level={'Average'} bg={blue[600]} />
+            </Grid>
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <EthereumGas gwei={gasTracker?.FastGasPrice} level={'Fast'} bg={red[600]} />
+            </Grid>
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <EthereumGas gwei={gasTracker?.LastBlock} level={'Block'} bg={orange[600]} />
+            </Grid>
 
             <Grid item lg={3} sm={6} xl={3} xs={12}>
               <Budget />
