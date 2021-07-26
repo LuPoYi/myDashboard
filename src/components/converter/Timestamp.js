339a97e672346150cdd3dc28a5cf01b59424a1d2
Bob <maydaybob2000@gmail.com>
2021-07-26 20:53:57 +0800

    add GMT time to Timestamp.js

--- src/components/converter/Timestamp.js
+++ src/components/converter/Timestamp.js
@@ -2,16 +2,24 @@ import { useState } from 'react'
 import { Card, CardHeader, CardContent, Divider, Grid, TextField } from '@material-ui/core'
 
 const Timestamp = () => {
+  const [dateGMT, setDateGMT] = useState(new Date().toUTCString())
   const [date, setDate] = useState(Date())
   const [timestamp, setTimestamp] = useState(Math.floor(Date.now() / 1000))
+
+  const dateGMTHandleChange = (e) => {
+    setDateGMT(e.target.value)
+    setDate(new Date(Date.parse(e.target.value)))
+  }
 
   const timestampHandleChange = (e) => {
     setTimestamp(e.target.value)
     setDate(new Date(parseInt(e.target.value) * 1000))
+    setDateGMT(new Date(parseInt(e.target.value) * 1000).toUTCString())
   }
 
   const dateHandleChange = (e) => {
     setDate(e.target.value)
+    setDateGMT(new Date(e.target.value).toUTCString())
     setTimestamp(Date.parse(e.target.value) / 1000)
   }
 
@@ -34,7 +42,17 @@ const Timestamp = () => {
           <Grid item md={12} xs={12}>
             <TextField
               fullWidth
-              label="Date"
+              label="Date(GMT)"
+              variant="outlined"
+              onChange={dateGMTHandleChange}
+              value={dateGMT}
+            />
+          </Grid>
+
+          <Grid item md={12} xs={12}>
+            <TextField
+              fullWidth
+              label="Date(+8)"
               variant="outlined"
               onChange={dateHandleChange}
               value={date}
