644a4398af5ae34b78b68ac6b56758dddcd30f6f
Bob <maydaybob2000@gmail.com>
2021-11-05 17:52:13 +0800

    add progress bar

--- src/pages/DeFiPortfolio.js
+++ src/pages/DeFiPortfolio.js
@@ -1,13 +1,54 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Helmet } from 'react-helmet'
-import Web3 from 'web3'
-import { Box, Container, Grid } from '@material-ui/core'
-import DeFiToolbar from 'src/components/defi/DeFiToolbar'
+import { Typography, Box, Container, Grid } from '@material-ui/core'
+
+import './progressBar.css'
 
 const DeFiPortfolio = () => {
-  const [address, setAddress] = useState('')
+  // https://api.binance.com/api/v3/ticker/price?symbol=BNBUSDT
+  // https://api.binance.com/api/v3/ticker/price?symbol=SOLUSDT
+  // https://api.coingecko.com/api/v3/simple/price?ids=solana%2Cbinancecoin&vs_currencies=usd&include_market_cap=true
 
-  const disconnectOnClick = () => {}
+  const [marketCap, setMarketCap] = useState({
+    binancecoin: { usd: 0, usd_market_cap: 0 },
+    solana: { usd: 0, usd_market_cap: 0 }
+  })
+
+  useEffect(() => {
+    fetch(
+      'https://api.coingecko.com/api/v3/simple/price?ids=solana%2Cbinancecoin&vs_currencies=usd&include_market_cap=true'
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          if (result) setMarketCap(result)
+        },
+        (error) => console.log('fetch api error', error)
+      )
+  }, [])
+
+  const progressBar = (
+    <div className="wrapper">
+      <div className="green">
+        <div className="progress">
+          <div className="inner">
+            <div className="percent">
+              <span>
+                {(
+                  (marketCap?.['solana']?.['usd_market_cap'] /
+                    marketCap?.['binancecoin']?.['usd_market_cap']) *
+                  100
+                ).toFixed(1)}
+              </span>
+              %
+            </div>
+            <div className="water"></div>
+            <div className="glare"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 
   return (
     <>
@@ -20,7 +61,7 @@ const DeFiPortfolio = () => {
           minHeight: '100%',
           py: 3
         }}>
-        <Container maxWidth="lg">
+        {/* <Container maxWidth="lg">
           <Grid container spacing={3}>
             <Grid item lg={3} md={6} xs={12}>
               ETH: 0x...
@@ -35,6 +76,19 @@ const DeFiPortfolio = () => {
               SOL: ...
             </Grid>
           </Grid>
+        </Container> */}
+        <Container maxWidth="lg">
+          <Typography color="textPrimary" variant="h2">
+            BNB vs SOL
+          </Typography>
+          <Typography color="textSecondary" variant="h6">
+            BNB: {marketCap?.['binancecoin']?.['usd']} -{' '}
+            {marketCap?.['binancecoin']?.['usd_market_cap']}
+          </Typography>
+          <Typography color="textSecondary" variant="h6">
+            SOL {marketCap?.['solana']?.['usd']} - {marketCap?.['solana']?.['usd_market_cap']}
+          </Typography>
+          {progressBar}
         </Container>
       </Box>
     </>
