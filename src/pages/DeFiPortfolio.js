fcc1b0525c14988fc6f5d70eba778f5adfc4eb96
Bob <maydaybob2000@gmail.com>
2021-11-05 18:09:35 +0800

    fix progress bar by better ui

--- src/pages/DeFiPortfolio.js
+++ src/pages/DeFiPortfolio.js
@@ -5,14 +5,11 @@ import { Typography, Box, Container, Grid } from '@material-ui/core'
 import './progressBar.css'
 
 const DeFiPortfolio = () => {
-  // https://api.binance.com/api/v3/ticker/price?symbol=BNBUSDT
-  // https://api.binance.com/api/v3/ticker/price?symbol=SOLUSDT
-  // https://api.coingecko.com/api/v3/simple/price?ids=solana%2Cbinancecoin&vs_currencies=usd&include_market_cap=true
-
   const [marketCap, setMarketCap] = useState({
     binancecoin: { usd: 0, usd_market_cap: 0 },
     solana: { usd: 0, usd_market_cap: 0 }
   })
+  const [ratio, setRatio] = useState(0)
 
   useEffect(() => {
     fetch(
@@ -27,20 +24,23 @@ const DeFiPortfolio = () => {
       )
   }, [])
 
+  useEffect(() => {
+    setRatio(
+      (
+        (marketCap?.['solana']?.['usd_market_cap'] /
+          marketCap?.['binancecoin']?.['usd_market_cap']) *
+        100
+      ).toFixed(1)
+    )
+  }, [marketCap])
+
   const progressBar = (
     <div className="wrapper">
-      <div className="green">
+      <div className={ratio > 66 ? 'green' : ratio > 33 ? 'orange' : 'red'}>
         <div className="progress">
           <div className="inner">
             <div className="percent">
-              <span>
-                {(
-                  (marketCap?.['solana']?.['usd_market_cap'] /
-                    marketCap?.['binancecoin']?.['usd_market_cap']) *
-                  100
-                ).toFixed(1)}
-              </span>
-              %
+              <span>{ratio}</span>%
             </div>
             <div className="water"></div>
             <div className="glare"></div>
@@ -49,6 +49,19 @@ const DeFiPortfolio = () => {
       </div>
     </div>
   )
+
+  const convertToInternationalCurrencySystem = (number) => {
+    // Nine Zeroes for Billions
+    return Math.abs(Number(number)) >= 1.0e9
+      ? (Math.abs(Number(number)) / 1.0e9).toFixed(2) + 'B'
+      : // Six Zeroes for Millions
+      Math.abs(Number(number)) >= 1.0e6
+      ? (Math.abs(Number(number)) / 1.0e6).toFixed(2) + 'M'
+      : // Three Zeroes for Thousands
+      Math.abs(Number(number)) >= 1.0e3
+      ? (Math.abs(Number(number)) / 1.0e3).toFixed(2) + 'K'
+      : Math.abs(Number(number))
+  }
 
   return (
     <>
@@ -61,33 +74,29 @@ const DeFiPortfolio = () => {
           minHeight: '100%',
           py: 3
         }}>
-        {/* <Container maxWidth="lg">
+        <Container maxWidth="lg">
           <Grid container spacing={3}>
-            <Grid item lg={3} md={6} xs={12}>
-              ETH: 0x...
-            </Grid>
-            <Grid item lg={3} md={6} xs={12}>
-              BSC: 0x...
-            </Grid>
-            <Grid item lg={3} md={6} xs={12}>
-              MATIC: 0x...
-            </Grid>
-            <Grid item lg={3} md={6} xs={12}>
-              SOL: ...
+            <Grid item md={6} xs={12}>
+              <Typography color="textPrimary" variant="h2">
+                BNB ${marketCap?.['binancecoin']?.['usd']}
+              </Typography>
+              <Typography color="textSecondary" gutterBottom variant="h6">
+                market cap:{' '}
+                {convertToInternationalCurrencySystem(
+                  marketCap?.['binancecoin']?.['usd_market_cap']
+                )}
+              </Typography>
             </Grid>
           </Grid>
-        </Container> */}
-        <Container maxWidth="lg">
-          <Typography color="textPrimary" variant="h2">
-            BNB vs SOL
-          </Typography>
-          <Typography color="textSecondary" variant="h6">
-            BNB: {marketCap?.['binancecoin']?.['usd']} -{' '}
-            {marketCap?.['binancecoin']?.['usd_market_cap']}
-          </Typography>
-          <Typography color="textSecondary" variant="h6">
-            SOL {marketCap?.['solana']?.['usd']} - {marketCap?.['solana']?.['usd_market_cap']}
-          </Typography>
+          <Grid item md={6} xs={12}>
+            <Typography color="textPrimary" variant="h2">
+              SOL ${marketCap?.['solana']?.['usd']}
+            </Typography>
+            <Typography color="textSecondary" gutterBottom variant="h6">
+              market cap:{' '}
+              {convertToInternationalCurrencySystem(marketCap?.['solana']?.['usd_market_cap'])}
+            </Typography>
+          </Grid>
           {progressBar}
         </Container>
       </Box>
