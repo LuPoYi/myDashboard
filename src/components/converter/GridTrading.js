da5b54ea74652dc8bb89acc9637608a1070bb1d1
Bob <maydaybob2000@gmail.com>
2022-10-20 11:08:52 +0800

    Update GridTrading

--- src/components/converter/GridTrading.js
+++ src/components/converter/GridTrading.js
@@ -1,32 +1,63 @@
 import React, { useEffect, useState } from 'react'
 
-import { Card, CardContent, CardHeader, Divider, Grid, TextField } from '@material-ui/core'
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  TextField
+} from '@material-ui/core'
+import { Decimal } from 'decimal.js'
 
 function GridTrading() {
-  const [high, setHigh] = useState(12)
-  const [low, setLow] = useState(4)
+  const [high, setHigh] = useState(new Decimal(12))
+  const [low, setLow] = useState(new Decimal(4))
   const [grid, setGrid] = useState(51)
   const [step, setStep] = useState()
-  const [result, setResult] = useState({})
+  const [result, setResult] = useState()
 
-  const handleHighChange = (e) => setHigh(Number(e.target.value))
-  const handleLowChange = (e) => setLow(Number(e.target.value))
-  const handleGridChange = (e) => setGrid(Number(e.target.value))
+  const handleHighChange = (e) => {
+    try {
+      setHigh(new Decimal(e.target.value))
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  const handleLowChange = (e) => {
+    try {
+      setLow(new Decimal(e.target.value))
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  const handleGridChange = (e) => {
+    try {
+      setGrid(Number(e.target.value))
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
   useEffect(() => {
     let _result = []
-    const _step = (high - low) / (grid - 1)
-    setStep(_step)
-    _result.push(low)
     let _price = low
+    const _step = high.minus(low).dividedBy(grid - 1)
+
+    _result.push(Number(_price))
     for (let i = 1; i < grid; i++) {
-      _price += _step
-      _result.push(_price)
+      _price = _price.add(_step)
+      _result.push(Number(_price))
     }
 
     setResult(_result)
     setStep(_step)
   }, [high, low, grid])
+
   return (
     <Card>
       <CardHeader title="Grid Trading" />
@@ -61,9 +92,16 @@ function GridTrading() {
             />
           </Grid>
           <Grid item md={12} xs={12}>
-            {JSON.stringify(result)}
-            <br />
-            {step}
+            Step: {Number(step)}
+            <List sx={{ bgcolor: 'aliceblue', height: 300, overflowY: 'auto' }}>
+              {result?.map((item, index) => (
+                <ListItem key={item} disablePadding secondaryAction={`#${index + 1}`}>
+                  <ListItemButton>
+                    <ListItemText primary={item} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
           </Grid>
         </Grid>
       </CardContent>
